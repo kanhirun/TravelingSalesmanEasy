44c0fb31e8aa61fb09ae95bde82704905e49bc26
Kel Kanhirun <kkanhirun@gmail.com>
2015-04-13 00:52:23 -0500

    feat: first sketch of the solution to pass test 1

--- lib/traveling_salesman_easy.py
+++ lib/traveling_salesman_easy.py
@@ -1,4 +1,22 @@
 class TravelingSalesmanEasy(object):
 
   def getMaxProfit(self, nCities, profit, city, visits):
-    return 10
+    totalProfits = 0
+    soldItems    = []  # items which have already been sold 
+
+    for visit in visits:
+      for (item, itemCityId) in enumerate(city):
+        if (item in soldItems):
+          continue
+
+        soldItem = 0 
+        runningMaxProfit = 0
+
+        if (itemCityId == visit) and (profit[item] > runningMaxProfit):
+          soldItem = item 
+          runningMaxProfit = profit[item]
+
+      totalProfits += runningMaxProfit
+      soldItems.append(soldItem)
+
+    return totalProfits
